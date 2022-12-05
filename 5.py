ab16223e6b61adac44ad344a5002d79110dde062
Corentin Normand <corentin.normand@swan.io>
2022-12-05 11:41:16 +0100

    day 5

--- 5.py
+++ 5.py
@@ -17,67 +17,56 @@ def parse_stacks(stacks: list[str]):
     return (piles)
 
 
-def parse_move(move):
+def parse_move(move: str):
     moves = move.split(" ")
     return (int(moves[1]), int(moves[3]), int(moves[5]))
 
 
-def part1():
+def parse_lines(lines: list[str]):
     stacks = []
     moves = []
-    parsed_stacks = []
+    parsed_stacks = [str]
     is_part1 = True
     for line in lines:
         if len(line) == 0:
             is_part1 = False
             parsed_stacks = parse_stacks(stacks)
             continue
-
         if is_part1:
             stacks.insert(0, line)
         else:
             moves.append(parse_move(line))
+    return (parsed_stacks, moves)
+
+
+def result(stacks):
+    return "".join([stacks[r][-1:][0] for r in stacks])
+
+
+def part1():
+
+    stacks, moves = parse_lines(lines)
 
     for move in moves:
         _amount, _from, _to = move
-        to_add = parsed_stacks[_from][-_amount:]
+        to_add = stacks[_from][-_amount:]
         to_add.reverse()
-        parsed_stacks[_to].extend(to_add)
-        parsed_stacks[_from] = parsed_stacks[_from][0:-_amount]
+        stacks[_to].extend(to_add)
+        stacks[_from] = stacks[_from][0:-_amount]
 
-    res = ""
-    for result in parsed_stacks:
-        res += parsed_stacks[result][-1:][0]
+    print(result(stacks))
 
-    print(res)
+
 
 def part2():
-    stacks = []
-    moves = []
-    parsed_stacks = []
-    is_part1 = True
-    for line in lines:
-        if len(line) == 0:
-            is_part1 = False
-            parsed_stacks = parse_stacks(stacks)
-            continue
-
-        if is_part1:
-            stacks.insert(0, line)
-        else:
-            moves.append(parse_move(line))
-
+    stacks, moves = parse_lines(lines)
     for move in moves:
         _amount, _from, _to = move
-        to_add = parsed_stacks[_from][-_amount:]
-        parsed_stacks[_to].extend(to_add)
-        parsed_stacks[_from] = parsed_stacks[_from][0:-_amount]
+        to_add = stacks[_from][-_amount:]
+        stacks[_to].extend(to_add)
+        stacks[_from] = stacks[_from][0:-_amount]
 
-    res = ""
-    for result in parsed_stacks:
-        res += parsed_stacks[result][-1:][0]
-
-    print(res)
+    print(result(stacks))
 
 
 part1()
